43086df410a9b4d9d0d9cd357b5b88ffb039f436
jdprada1760 <jd.prada1760@uniandes.edu.co>
2014-09-23 17:13:57 -0500

    Lector de posiciones y velocidade, (Errores de impresion)

--- reader.c
+++ reader.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#define man "./reader.x toRead toWrite"
 
 void readBinary(char* name);
 
 // Estructura que contiene el header (256 bytes)
 struct Header{
-  int npart[6];
+  unsigned int npart[6];
   double mass[6];
   double time;
   double redshift;
@@ -20,45 +21,58 @@ struct Header{
   double Omega0;
   double OmegaLambda;
   double HubbleParam;
-  char fill[256 - 6 * 4 - 6 * 8 - 2 * 8 - 2 * 4 - 6 * 4 - 2 * 4 - 4 * 8];	/* fills to 256 Bytes */
-} header;
+  int flag_stellarAge;
+  int flag_metals;
+  int numPart_total_hw;
+  int flag_entropy_ics;
+  char fill[256 - sizeof(unsigned int) - 7*sizeof(double) - 9*sizeof(int)];	/* fills to 256 Bytes */
+};
 
 // Estructura que contiene la información importante de las partículas
 struct Particle{
-  float Pos[3];
-  float Vel[3];
+  float* Pos;
+  float* Vel;
   float Mass;
   int Type;
-} *P;
+};
 
 // Crea el contenedor de IDs
-int *Id;
+int* Id;
+int numP = 0; //Numero de Particulas
+struct Header header;
+struct Particle* P;
 
-int main(){
+int main(int argc, char **argv){
+  printf("Begin...\n");
+  readBinary( argv[1] );
+  FILE *fila = fopen( argv[2], "w" );
+  // Imprime un archivo con las posiciones, velocidades, masas y IDs de las partículas
+  int i;
+  printf("Writing...\n");
+  i = 0;
+  fprintf( fila, "Archivo\n");
 
-
-
+  for( i = 0; i < numP; i++ ){
+    printf("%d\n",i);
+    fprintf( fila, "%d %f %f %f %f %f %f\n", Id[i], P[i].Pos[0], P[i].Pos[1], P[i].Pos[2], P[i].Vel[0] ,P[i].Vel[1] ,P[i].Vel[2] );
+  }
+  fclose(fila);
   return 0;
 }
 
 void readBinary( char* name ){
- 
+  FILE *fila = fopen("phase_Space.dat", "w");
   // Abre el archivo para leer en binario
-  FILE* file = fopen( name, "rb" );
+  printf("Opening file...\n");
+  FILE* file = fopen( name, "r" );
   if( !file ){
     printf( "Unable to open %s", name );
     fclose(file);
     return;
   }
 
-  // Obtiene el tamaño del archivo
-  unsigned long length;
-  fseek( file, 0, SEEK_END);
-  length = ftell( file );
-  fseek( file, 0, SEEK_SET ); 
-
   // Abre y cierra los bloques 
-# define SKIP fread(&dummy, sizeof(dummy), 1, file);
+# define SKIP fread(&dummy, sizeof(dummy), 1, file)
 
   //-------------------------------------------------------------
   //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -68,8 +82,7 @@ void readBinary( char* name ){
 
   
   int dummy; // Skip
-  int numP = 0; // Numero de particulas
-  int i,j,k; // Indices
+  int i,j; // Indices
 
   //-------------------------------------------------------------
   //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -77,20 +90,27 @@ void readBinary( char* name ){
   //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   //-------------------------------------------------------------
 
-
+  printf("Geting header...\n");
   SKIP; // Abre el bloque
   fread( &header, sizeof(struct Header), 1, file );
   SKIP; // Cierra el bloque
 
   // Obtiene el numero de particulas
-  for( k = 0; k < 6; k++){
-    numP += header.npart[k];
-  }
+  printf("Getting number of particles...\n");
+  numP += header.npart[5];
+  printf("%d\n", header.npart[5]);
+  
 
   // Aparta memoria
+  printf("Allocating memory...\n");
   P = malloc(numP*sizeof(struct Particle));
   Id = malloc(numP*sizeof(int));
-  
+  for( i = 0; i < numP; i++){
+    P[i].Pos = malloc(3*sizeof(float));
+    P[i].Vel = malloc(3*sizeof(float));
+  }
+  printf("%d\n", numP);
+
   //-------------------------------------------------------------
   //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   // Reader posiciones-velocidades-masas-ids
@@ -98,40 +118,52 @@ void readBinary( char* name ){
   //-------------------------------------------------------------
 
   // Posiciones
+  printf("Getting Positions...\n");
   SKIP;
   for( i = 0; i < numP; i++){
-    fread( &P[i].Pos, sizeof(P[i].Pos), 1, file );
+    for( j = 0; j < 3; j++){
+      fread( &P[i].Pos[j], sizeof(float), 1, file );
+      fprintf(fila, "%f\n", P[i].Pos[j]);
+    }
   }
   SKIP;
 
   // Velocidades
+  printf("Getting Velocities...\n");
   SKIP;
   for( i = 0; i < numP; i++){
-    fread( &P[i].Vel, sizeof(P[i].Vel), 1, file );
+    for( j = 0; j < 3; j++){
+      fread( &P[i].Vel[j], sizeof(float), 1, file );
+    }
   }
   SKIP;
 
   // IDs
+  printf("Getting Particle IDs...\n");
   SKIP;
   fread( &Id, sizeof(Id), 1, file );
   SKIP;
 
   // Masas
+  printf("Getting Masses...\n");
   SKIP;
-  for( i = 0; i < 6; i++){
-    int sum = 0;
-    if( header.mass[i] == 0 ){
-      for( j = 0; j < header.npart[i]; j++ ){
-	fread( &P[sum + j].Mass, sizeof(P[i].Mass), 1, file );
-      }
+  int sum = 0;
+  i = 5;
+  if( header.mass[i] == 0 ){
+    for( j = 0; j < header.npart[i]; j++ ){
+      fread( &P[sum].Mass, sizeof(float), 1, file );
+      sum++;
     }
-    else{
-      for( j = 0; j < header.npart[i]; j++ ){
-	P[sum +j].Mass = header.mass[i];
-      }
-    }
-    sum += header.npart[i];
   }
+  else{
+    for( j = 0; j < header.npart[i]; j++ ){
+      P[sum].Mass = header.mass[i];
+    }
+  }
+  sum++;
   SKIP;
+  printf("End...\n");
+  fclose(file);
+  fclose(fila);
 }
 
