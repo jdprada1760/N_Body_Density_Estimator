ed0f2a5f52bc33483176dab3818824d0932669c4
jdprada1760 <jd.prada1760@uniandes.edu.co>
2015-02-24 15:10:08 -0500

    Concatenar listas

--- C_Methods/Tree.c
+++ C_Methods/Tree.c
@@ -1,6 +1,5 @@
 /*
  * Arbol ( con su respectiva lista )
- *
  */
 
 #include <stdio.h>
@@ -9,11 +8,16 @@
 #include <math.h>
 #include <time.h>
 
+
+/*
+ * Metodos
+ */
+void addToList(int i);
+
 /*
  * Lista simplemente encadenada
- *
  */
-struct List{
+typedef struct List{
   // El hijo proximo
   struct List next;
   // El ultimo hijo
@@ -27,24 +31,34 @@ struct List{
 
 /*
  * Arbol
- *
  */
+typedef struct Nodo{
+  // El padre
+  Struct Nodo dad;
+  // El hijo izquierdo
+  struct Nodo left;
+  // El ultimo hijo
+  struct Nodo right;
+  // La lista que guarda los tetraedros contenidos en el espacio definido por este nodo
+  struct List thdrons;
+  // El numero de tetraedros que hay de este nodo en adelante
+  unsigned int n;
+  // Inicio del espacio
+  float ini;
+  // Final del espacio
+  float fin;
+  // La dimension de restriccion: es un entero   n%3
+  int dim;
+};
 
- struct Nodo{
-   // El padre
-   Struct Nodo dad;
-   // El hijo izquierdo
-   struct Nodo left;
-   // El ultimo hijo
-   struct Nodo right;
-   // La lista que guarda los tetraedros contenidos en el espacio definido por este nodo
-   struct List thdrons;
-   // El numero de tetraedros que hay de este nodo en adelante
-   unsigned int n;
-   // Inicio del espacio
-   float ini;
-   // Final del espacio
-   float fin;
-   // La dimension de restriccion: es un entero   n%3
-   int dim;
- };
+/*
+ * Concatena 2 listas
+ */
+void cat(List lista, List lista2){
+  // Aumenta el numero de elementos en la lista primera.
+  lista.n = lista.n + lista2.n;
+  // Crea el elemento a la lista
+  // Añade el nuevo elemento en la cola y cambia la referencia al elemento final en el primer nodo
+  lista.last.next = lista2;
+  lista.last = lista2.last;
+}
