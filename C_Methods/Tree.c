eca78f1fdd5a47a280213d139141b102ab593736
jdprada1760 <jd.prada1760@uniandes.edu.co>
2015-03-10 19:36:51 -0500

    Lista funcionando, al fin!

--- C_Methods/Tree.c
+++ C_Methods/Tree.c
@@ -11,7 +11,7 @@
 /*
  * Lista simplemente encadenada
  */
-typedef struct List{
+struct List{
   // El hijo proximo
   struct List* next;
   // El ultimo hijo
@@ -21,12 +21,13 @@ typedef struct List{
   int index;
   // Guarda la longitud de la lista (solo la guarda el primero, o ahi vemos)
   unsigned int n;
-}List;
+};
+typedef struct List List;
 
 /*
  * Arbol
  */
-typedef struct Nodo{
+struct Nodo{
   // El padre
   //struct Nodo dad;
   // El hijo izquierdo
@@ -34,67 +35,118 @@ typedef struct Nodo{
   // El ultimo hijo
   struct Nodo* right;
   // La lista que guarda los tetraedros contenidos en el espacio definido por este nodo
-  struct List* thdrons;
+  struct List thdrons;
   // El numero de tetraedros que hay de este nodo en adelante
-  unsigned int n;
+  //unsigned int n;
   // Inicio del espacio
   float ini;
   // Final del espacio
   float fin;
   // La dimension de restriccion: es un entero   n%3
   //int dim;
-}Nodo;
+};
+typedef struct Nodo Nodo;
+
+//------------------------------------------------------------------------------
+//--- Metodos inicializadores---------------------------------------------------
+//------------------------------------------------------------------------------
 
 /*
- * Concatena 2 listas
+ * Inicializa un nodo vacio
+Nodo iniNodo(){
+  Nodo nodo;
+  nodo.left = &nodoI;
+  nodo.right = &nodoD;
+  nodo.thdrons = lista;
+  nodo.n = n;
+  nodo.ini = ini;
+  nodo.fin = fin;
+  return nodo;
+}
+*/
+
+/*
+ * Inicializa una lista vacía dado un índice;
  */
-void cat(List lista,struct List lista2){
+List* iniList( int index ){
+  List *li = malloc(sizeof(List));
+  li->next = 0;
+  li->n = 1;
+  li->index = index;
+  li->last = li;
+  return li;
+}
+
+
+
+
+/*
+ * Concatena lista2 a lista
+ */
+void cat(List* lista, List* lista2){
   // Aumenta el numero de elementos en la lista primera.
-  lista.n = lista.n + lista2.n;
+  lista->n = lista->n + lista2->n;
   // Crea el elemento a la lista
   // Añade el nuevo elemento en la cola y cambia la referencia al elemento final en el primer nodo
-  lista.last.next = *lista2;
-  lista.last = *lista2.last;
+  (lista->last)->next = lista2;
+  lista->last = lista2->last;
 }
 
 /*
  * Crea un arbol, binario altura L
- */
 struct Nodo mkTree(int L,Nodo ini){
    if( L == 0){
      return ini;
    }
    else{
      L--;
-     struct Nodo ini2 = ini;
-     struct Nodo new =  {ini,ini2, NULL,0,0,0};
+     Nodo ini2 = ini;
+     Nodo new;
      new.left = &ini;
      new.right = &ini2;
      return mkTree(L,new);
    }
+}*/
+
+void printList(List* lista){
+  List *l = lista;
+  do{
+    printf("-->%d\n", l->index );
+    l = l->next;
+  }while(l != 0);
 }
 
-void printList(List lista){
-  Lista l = lista;
-  do{
-    printf("-->%d\n", l.index );
-    l = lista.next;
-  }while(l != NULL);
-}
 
 int main(){
-  struct List lista1 = {NULL,NULL, 1, 3};
-  lista1.next = {NULL,NULL, 3, 3};
-  struct Lista temp = {NULL,NULL, 5, 3};
-  lista1.next.next = temp;
-  lista1.last = temp;
-  struct List lista2 = {NULL,NULL, 2, 3};
-  lista2.next = {NULL,NULL, 4, 3};
-  temp = {NULL,NULL, 6, 3};
-  lista2.next.next = temp;
-  lista2.last = temp;
+
+  // Prueba de inilist
+  List *lista1 = iniList(1);
+  List *lista2 = iniList(3);
+  printf("Lista1:\n");
   printList(lista1);
+  printf("Lista2:\n");
   printList(lista2);
-  printList(cat(lista1,lista2));
+  printf("Longitud: L1=%d, L2=%d\n", lista1->n, lista2->n);
+
+    ////////////////////////////////////////////////////////////////
+   ////////////////////// IMPORTANTE!!!////////////////////////////
+  ////////////////////////////////////////////////////////////////
+
+  // Asi se pasa de un nodo a otro
+  lista1 = lista1->last;
+
+    ////////////////////////////////////////////////////////////////
+   ////////////////////// IMPORTANTE!!!////////////////////////////
+  ////////////////////////////////////////////////////////////////
+  printf("Lista1:\n");
+  printf("Longitud: L1=%d\n", lista1->n);
+  printList(lista1);
+
+  // Prueba del método cat
+  cat(lista1,lista2);
+  printf("Lista1:\n");
+  printf("Longitud: L1=%d\n", lista1->n);
+  printList(lista1);
+
   return 0;
 }
